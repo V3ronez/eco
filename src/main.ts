aacabccfe011ab0fee0cce511492aea2965e218a
V3ronez <v3ronez.dev@gmail.com>
2022-12-19 00:06:48 -0300

    swagger add

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,21 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Eco notifications Documentation')
+    .setDescription(
+      'This is microservice Eco. Eco is a notification service  build in Ignite Lab (NodeJs)',
+    )
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api-doc', app, document);
   await app.listen(3000);
 }
 bootstrap();
